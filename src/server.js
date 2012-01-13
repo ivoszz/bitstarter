7b8a64a999503d0ffe789aab56b4f2c2c0cc152f
ivoszz <ivoszz@gmail.com>
2012-01-13 21:24:45 +0100

    added connection to shared database
    Signed-off-by: ivoszz <ivoszz@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,4 +1,12 @@
+var pg = require('pg');
 var express = require('express');
+
+console.log(process.env.DATABASE_URL);
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM table');
+  console.log(query);
+});
 
 var app = express.createServer(express.logger());
 
