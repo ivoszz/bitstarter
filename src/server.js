8304db874d36e5ebc6af095e8b368ae179add9fb
ivoszz <ivoszz@gmail.com>
2012-01-14 00:56:01 +0100

    added bootstrap 2.0 support
    Signed-off-by: ivoszz <ivoszz@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,20 +1,38 @@
 var pg = require('pg');
 var express = require('express');
 
+/*
 console.log(process.env.DATABASE_URL);
 
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   var query = client.query('SELECT * FROM table');
   console.log(query);
-});
+}); */
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', { layout: false });
+
+app.configure(function() {
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-var port = process.env.PORT || 3000;
+var pageInfo = {
+  pageTitle: 'Go klub Brno'
+};
+
+app.get('/', function(request, response) {
+  response.render('index', { pageInfo: pageInfo });
+});
+
+app.get('/players', function(request, response) {
+  response.render('players', { pageInfo: pageInfo })
+});
+
+var port = process.env.PORT || 4000;
 
 app.listen(port);
-console.log('Listening on %d', port);
+console.log('Listening on port %d.', port);
